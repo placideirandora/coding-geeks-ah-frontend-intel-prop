e5d895567cbcde748aa2bae017df87e5ccc2b0b7
Musinda Kadhuwa <kadhuwa.musinda@andela.com>
2019-09-24 10:20:13 +0200

    chore(Setup Redux): setup redux [Starts #167484540]

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import logo from './logo.png';
 import './App.css';
 
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Authors Haven by coding geeks.
-        </p>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Authors Haven by coding geeks.</p>
+        </header>
+      </div>
+    </Provider>
   );
 }
 
