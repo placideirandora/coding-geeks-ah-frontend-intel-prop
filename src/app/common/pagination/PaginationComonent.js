1ed3a08345ee1a36c57941cf2663c3b1be74e8ff
harerakalex <hareraloston@gmail.com>
2019-10-30 20:26:52 +0200

    feat(pagination): add pagination support

---
+++ src/app/common/pagination/PaginationComonent.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Pagination from 'react-js-pagination';
+import './Pagination.scss';
+
+export class PaginationComonent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 1,
+    };
+  }
+
+  handlePageChange = (pageNumber) => {
+    const { getAllArticles } = this.props;
+    getAllArticles(pageNumber);
+    this.setState({ activePage: pageNumber });
+  };
+
+  render() {
+    const { activePage } = this.state;
+    const { articleNumbers } = this.props;
+    return (
+      <div>
+        <div className="pagination-container">
+          <Pagination
+            activePage={activePage}
+            itemsCountPerPage={10}
+            totalItemsCount={articleNumbers}
+            pageRangeDisplayed={3}
+            onChange={this.handlePageChange}
+            hideNavigation
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PaginationComonent;
